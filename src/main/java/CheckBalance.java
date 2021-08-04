cc9aff97cdc61668cfa3b8d90db537c2f87df14d
Natalija Hamandikova <test@test.lv>
2021-08-04 20:39:15 +0300

    get user info added

--- src/main/java/CheckBalance.java
+++ src/main/java/CheckBalance.java
@@ -5,9 +5,10 @@ public class CheckBalance {
     public static void main(String[] args) {
         try {
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://127.0.0.1:3306/bank", "root", "danaja05");
+                    "jdbc:mysql://127.0.0.1:3306/Bank", "root", "Oxford1984");
             System.out.println(checkBalance(con, 2));
             madeTransaction(con, 2,3, 20);
+            getInfo(con, 2);
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -45,5 +46,17 @@ public class CheckBalance {
             err.printStackTrace();
         }
         return accountBalance;
+    }
+    public static void getInfo(Connection connection, int id) {
+        Statement statement = null;
+        String showUserInfo = "select accounts.balance, accounts.account_number, users.name, users.id from users inner join accounts on accounts.user_id = users.id where users.id =" + id;
+        try {
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(showUserInfo);
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + " " + rs.getInt(4));
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
     }
 }
